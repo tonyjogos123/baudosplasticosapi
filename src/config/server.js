73f711277d247e3802ef8c4364ae0dbb4e59511a
Tony Lucas <tonyjogos123@gmail.com>
2020-07-24 18:00:33 -0300

    começo de routes nota

--- src/config/server.js
+++ src/config/server.js
@@ -11,9 +11,11 @@ app.use(bodyParser.urlencoded({extended:true}));
 const registerRoute = require('../routes/register');
 const loginRoute = require('../routes/login');
 const mercadoriaRoutes = require('../routes/mercadoria');
+const notasRoutes = require('../routes/notas');
 
 app.use('/register', registerRoute);
 app.use('/login',loginRoute);
 app.use('/mercadoria',mercadoriaRoutes);
+app.use('/notas',notasRoutes);
 
 app.listen(process.env.PORT || 3000);
