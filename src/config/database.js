105e264f98414e34332161f984e14e0bd1f736b4
Tony Lucas <tonyjogos123@gmail.com>
2020-07-24 13:02:06 -0300

    db asdas

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize('tiranotaapidb','adminbdpapi','55425610a',{
-    host:'mysql669.umbler.com:41890',
+    host:'mysql669.umbler.com',
     dialect:'mysql',
     raw:true,
     define: {
