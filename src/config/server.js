e945bff3f8bec0e51eeffc5cf5aeaf04c9bf00fe
Tony Lucas <tonyjogos123@gmail.com>
2020-07-25 18:14:07 -0300

    asdasda

--- src/config/server.js
+++ src/config/server.js
@@ -6,7 +6,6 @@ const cors = require('cors');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(express.methodOverride())
 app.use(cors());
 
 const registerRoute = require('../routes/register');
