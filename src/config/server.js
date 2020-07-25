f8e0aff210d5e838858bdfd571be9034e47d28a5
Tony Lucas <tonyjogos123@gmail.com>
2020-07-25 17:40:57 -0300

    asdasdas

--- src/config/server.js
+++ src/config/server.js
@@ -4,10 +4,10 @@ require('dotenv').config();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.methodOverride())
+app.use(cors());
 
 const registerRoute = require('../routes/register');
 const loginRoute = require('../routes/login');
