d3e3ceac37465dca67a1f8232f15a6ed48971de9
Tony Lucas <tonyjogos123@gmail.com>
2020-07-29 19:41:19 -0300

    asdasdas

--- src/models/MercadoriaVendida.js
+++ src/models/MercadoriaVendida.js
@@ -1,18 +1,18 @@
 const Sequelize = require('sequelize');
 const sequelize = require('../config/database');
 
-const MercadoriaVendida = sequelize.define('mercadoria_vendidas',{
-    id_mercadoria:Sequelize.INTEGER,
-    quantidade:Sequelize.INTEGER,
-    notaId:{
-        type:Sequelize.INTEGER,
-        references:{
-            model:'notas',
-            key:'id'
+const MercadoriaVendida = sequelize.define('mercadoria_vendidas', {
+    id_mercadoria: Sequelize.INTEGER,
+    quantidade: Sequelize.INTEGER,
+    notaId: {
+        type: Sequelize.INTEGER,
+        references: {
+            model: 'notas',
+            key: 'id'
         }
     }
 })
 
-MercadoriaVendida.sync({force:true})
+MercadoriaVendida.sync({ force: false })
 
 module.exports = MercadoriaVendida;
