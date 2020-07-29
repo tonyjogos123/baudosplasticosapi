185b97cc3afaa6428c926387d27c3af22014b3f4
Tony Lucas <tonyjogos123@gmail.com>
2020-07-29 14:34:06 -0300

    asdasdas

--- src/models/MercadoriaVendida.js
+++ src/models/MercadoriaVendida.js
@@ -3,7 +3,8 @@ const sequelize = require('../config/database');
 
 const MercadoriaVendida = sequelize.define('mercadoria_vendida',{
     id_mercadoria:Sequelize.INTEGER,
-    quantidade:Sequelize.INTEGER
+    quantidade:Sequelize.INTEGER,
+    id_nota:Sequelize.INTEGER
 })
 
 MercadoriaVendida.sync({force:false})
