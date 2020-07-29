f9d3ac883bafbfca7cd5c6a7ded60911c51bc168
Tony Lucas <tonyjogos123@gmail.com>
2020-07-29 13:42:37 -0300

    asdasd

--- src/routes/mercadoriaVendida.js
+++ src/routes/mercadoriaVendida.js
@@ -1,9 +1,37 @@
 const express = require('express');
 const router = express.Router();
-const mercadoriaVendida = require('../models/MercadoriaVendida');
+const MercadoriaVendida = require('../models/MercadoriaVendida');
+const login = require('../middleware/login');
 
-router.get('/',async(req,res) => {
-    
+router.get('/:token', login, async (req, res) => {
+    try {
+        const vendas = await MercadoriaVendida.findAll();
+        res.json({ success: true, vendas: vendas })
+    } catch (error) {
+        res.json({ success: false, message: error.message })
+    }
+})
+
+router.get('/:id/:token', login, async (req, res) => {
+    try {
+        const vendas = await MercadoriaVendida.findAll({ where: { id: req.params.id } });
+        res.json({ success: true, vendas: vendas })
+    } catch (error) {
+        res.json({ success: false, message: error.message })
+    }
+})
+
+router.post('/', login, async (req, res) => {
+    try {
+        const vendas = await MercadoriaVendida.create({
+            id_mercadoria: req.body.idMercadoria,
+            quantidade: req.body.quantidade,
+            id_nota: req.body.idNota
+        });
+        res.json({ success: true, s })
+    } catch (error) {
+        res.json({ success: false, message: error.message })
+    }
 })
 
 module.exports = router;
