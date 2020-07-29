fe44f88ffe4433bbb3ea686c8898bfe6bf4553b6
Tony Lucas <tonyjogos123@gmail.com>
2020-07-29 20:09:26 -0300

    asdasdas

--- src/models/Mercadoria.js
+++ src/models/Mercadoria.js
@@ -1,14 +1,14 @@
 const Sequelize = require('sequelize');
 const sequelize = require('../config/database');
 
-const Mercadoria = sequelize.define('mercadorias',{
-    nome:Sequelize.STRING,
-    precoCompra:Sequelize.DECIMAL(10,2),
-    precoVenda:Sequelize.DECIMAL(10,2),
-    nomeImg:Sequelize.STRING,
-    data:{type:Sequelize.DATE,defaultValue:Sequelize.NOW}
+const Mercadoria = sequelize.define('mercadorias', {
+    nome: Sequelize.STRING,
+    precoCompra: Sequelize.DECIMAL(10, 2),
+    precoVenda: Sequelize.DECIMAL(10, 2),
+    nomeImg: Sequelize.STRING,
+    data: { type: Sequelize.DATE, defaultValue: Sequelize.NOW }
 })
 
-Mercadoria.sync({force:false})
+Mercadoria.sync({ force: false })
 
 module.exports = Mercadoria;
