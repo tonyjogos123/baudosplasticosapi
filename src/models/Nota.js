0573bf486adc9e107e3c904564667295c7f7db05
Tony Lucas <tonyjogos123@gmail.com>
2020-07-30 11:35:32 -0300

    asdasda

--- src/models/Nota.js
+++ src/models/Nota.js
@@ -6,7 +6,7 @@ const d = new Date();
 
 const Nota = sequelize.define('notas', {
     total: Sequelize.FLOAT,
-    data: { type: Sequelize.DATE, defaultValue: Date.getMonth() }
+    data: { type: Sequelize.DATE, defaultValue: Date.now() }
 })
 
 Nota.hasMany(MercadoriaVendida);
