42b504479f922324329ad9079281fca2f59a9a9d
Tony Lucas <tonyjogos123@gmail.com>
2020-08-05 15:23:16 -0300

    asdasda

--- src/models/MercadoriaVendida.js
+++ src/models/MercadoriaVendida.js
@@ -13,6 +13,6 @@ const MercadoriaVendida = sequelize.define('mercadoria_vendidas', {
     }
 })
 
-MercadoriaVendida.sync({ force: true })
+MercadoriaVendida.sync({ force: false })
 
 module.exports = MercadoriaVendida;
