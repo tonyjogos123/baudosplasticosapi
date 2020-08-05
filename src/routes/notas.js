e86553aa5958e0629203dbd12c1461a191854fe9
Tony Lucas <tonyjogos123@gmail.com>
2020-08-05 09:54:42 -0300

    asdasdsa

--- src/routes/notas.js
+++ src/routes/notas.js
@@ -17,7 +17,7 @@ router.get('/:token', login, async (req, res) => {
 
 router.get("/limite/:limite/:token", async (req, res) => {
     try {
-        const notas = await Nota.findAll({ limit: req.params.limite,attributes:['id','total','data']});
+        const notas = await Nota.findAll();
         res.json({ success: true, notas: notas })
     } catch (error) {
         res.json({ success: false, erro: error.message })
