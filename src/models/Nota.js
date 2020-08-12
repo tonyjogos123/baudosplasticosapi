75a62faa8a074158231713d2e20db9ddcfd204d0
Tony Lucas <tonyjogos123@gmail.com>
2020-08-12 09:53:40 -0300

    sasdasda

--- src/models/Nota.js
+++ src/models/Nota.js
@@ -4,13 +4,13 @@ const Admin = require('./Admin');
 const MercadoriaVendida = require('./MercadoriaVendida');
 
 const Nota = sequelize.define('notas', {
-    total: {type:Sequelize.FLOAT,allowNull:false},
-    cliente:{type:Sequelize.STRING,allowNull:false},
+    total: { type: Sequelize.FLOAT, allowNull: false },
+    cliente: { type: Sequelize.STRING, allowNull: false },
     data: { type: Sequelize.DATEONLY, allowNull: false, defaultValue: Sequelize.NOW() }
 })
 
 Nota.hasMany(MercadoriaVendida);
 
-Nota.sync({ force: true })
+Nota.sync({ force: false })
 
 module.exports = Nota;
