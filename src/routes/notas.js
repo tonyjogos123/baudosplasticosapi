a2d0815fc3299ca8e623e01cb68aa0cda66768b6
Tony Lucas <tonyjogos123@gmail.com>
2020-08-12 12:20:16 -0300

    asdasda

--- src/routes/notas.js
+++ src/routes/notas.js
@@ -11,7 +11,7 @@ const { QueryTypes } = require('sequelize');
 
 router.get('/:token', login, async (req, res) => {
     try {
-        const notas = await Nota.findAll({order:["data","DESC"]});
+        const notas = await Nota.findAll();
         res.json({ success: true, notas: notas });
     } catch (error) {
         res.json({ success: false, erro: error.message })
