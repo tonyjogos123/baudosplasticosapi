0c9f3ceff9c15684dc3d2c9165e5029485beeb31
Tony Lucas <tonyjogos123@gmail.com>
2020-08-13 12:06:37 -0300

    asdasda

--- src/routes/notas.js
+++ src/routes/notas.js
@@ -48,7 +48,7 @@ router.get("/limite/:limite/:pulos/:token", login, async (req, res) => {
 
 router.get('/:datainicial/:datafinal/:token', login, async (req, res) => {
     try {
-        const notas = await sequelize.query({where: { data: { [Op.between]: [req.params.datainicial, req.params.datafinal] } } })
+        const notas = await Nota.findAll({ where: { data: { [Op.between]: [req.params.datainicial, req.params.datafinal] } }, order: [["data", "DESC"]] })
         res.json({ success: true, notas: notas })
     } catch (error) {
         res.json({ success: false, erro: error.message })
