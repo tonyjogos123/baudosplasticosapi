de26bc303c360c0edb8c6eb4e2ddb7cb6c51595c
Tony-CAPE <64075573+Tony-Lucas@users.noreply.github.com>
2021-03-04 15:36:19 -0300

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('tiranotaapidb','b148c18bfd1f4e','478ebbf4',{
+const sequelize = new Sequelize('heroku_5bcb46126675de9','b148c18bfd1f4e','478ebbf4',{
     host:'us-cdbr-east-03.cleardb.com',
     dialect:'mysql',
     raw:true,
